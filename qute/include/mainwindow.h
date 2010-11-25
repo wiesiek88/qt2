42f023111d616e3d45bdf0c8744fbae42faf7019
Mateusz Furgał <mateusz9@gmail.com>
2010-11-25 20:58:23 +0100

    das

--- qute/include/mainwindow.h
+++ qute/include/mainwindow.h
@@ -1,17 +1,7 @@
-/* Viewer  -- nowy plik */
-
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#ifndef VIEWER
-#include "include/renderarea.h"
-#include <QActionGroup>
-#endif
-
-class QLabel;
-class QScrollArea;
-class QScrollBar;
 
 namespace Ui {
     class MainWindow;
@@ -23,45 +13,12 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private slots:
-    #ifdef VIEWER
-    void open();
-    void zoomIn();
-    void zoomOut();
-    void normalSize();
-    void fitToWindow();
-
-    void greyScale();
-    void negative();
-    void gradient();
-    void hMirror();
-    void vMirror();
-    #else
-    void changePen();
-    #endif
-    void about();
-
-private:
-    void createActions();
-    Ui::MainWindow *ui;
     
-    #ifdef VIEWER
-    void mirror(bool hori);
-    void createMenus();
-    void updateActions();
-    void scaleImage(double factor);
-    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+private slots:
+    virtual void about();
 
-    QImage image;
-    QLabel *imageLabel;
-    QScrollArea *scrollArea;
-    double scaleFactor;
-    #else
-    RenderArea *renderArea;
-    QActionGroup *drawGroup;
-    #endif
+protected:
+    virtual void createActions();
 };
 
 #endif
